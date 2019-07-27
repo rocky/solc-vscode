e4efcdb041701eec2533c8ae157c269a9283d84e
rocky <rocky.bernstein@consensys.net>
2019-07-27 19:34:01 -0400

    Small message change

--- src/extension.ts
+++ src/extension.ts
@@ -76,7 +76,7 @@ export function activate(context: ExtensionContext) {
 
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "solidity-language-server" is now active!');
+  console.log('Congratulations, your Solidity extension, "solc-vscode", is now active!');
 
   // context.subscriptions.push(client);
 
